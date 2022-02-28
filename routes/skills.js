ed3928b5bcd2acf3ec30041221756124213bfcdb
Garam Jang <garamj88@gmail.com>
2022-02-28 01:01:32 -0500

    fix skills/index in views

--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,9 @@
 import { Router } from 'express'
+import * as skillsCtrl from '../controllers/skills.js'
 const router = Router()
 
 /* GET skills page. */
-router.get('/', function(req, res) {
-  res.send('respond') 
-})
+router.get('/', skillsCtrl.index)
 
 export { 
   router
